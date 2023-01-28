822335f263cc5209d8593f0fbce68ae9403c2f9b
Yuri Vinogradov <vvuri1978@gmail.com>
2023-01-28 20:49:25 +0300

    Add template API

---
+++ cmd/main/app.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"testAPI/internal/user"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	fmt.Fprintf(w, "Welcome %s!\n", name)
+	fmt.Println(r.URL)
+	fmt.Println(r.Header)
+	fmt.Println(r.ContentLength)
+}
+
+func main() {
+	log.Println("create router")
+	router := httprouter.New()
+
+	log.Println("register user")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	log.Println("start router")
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	// log.Fatal(http.ListenAndServe(":8080", router))
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second}
+
+	log.Fatal(server.Serve(listener))
+}
---
+++ internal/user/handler.go
@@ -0,0 +1,61 @@
+package user
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"testAPI/internal/handlers"
+)
+
+// как подксказка
+var _ handlers.Handler = &handler{}
+
+const (
+	usersURL = "/api/users"
+	userURL  = "/api/user:id"
+)
+
+type handler struct {
+}
+
+func NewHandler() handlers.Handler { // возвращаем интерфейс
+	return &handler{} // на самом деле структуру
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.GET(userURL, h.GetUserByUUID)
+	router.POST(usersURL, h.CreateUser)
+	router.PUT(userURL, h.UpgradeUser)
+	router.PATCH(userURL, h.PartiallyUpgradeUser)
+	router.DELETE(usersURL, h.DeleteUser)
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//TODO implement me
+	w.Write([]byte("API Get User"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//TODO implement me
+	w.Write([]byte("API Create User"))
+}
+
+func (h *handler) UpgradeUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//TODO implement me
+	w.Write([]byte("API Update User"))
+}
+
+func (h *handler) PartiallyUpgradeUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//TODO implement me
+	w.Write([]byte("API Partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//TODO implement me
+	w.Write([]byte("API Delete User"))
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//TODO implement me
+	w.Write([]byte("API List Users"))
+}
